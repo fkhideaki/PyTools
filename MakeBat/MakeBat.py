87f8ed05bbe1e86b3c2f989a5c2788855d69c7f9
fkhd <fkhideaki@gmail.com>
2026-02-08 10:27:45 +0900

    生成するpythonコマンドをpython launcherかフルパスを選択する機能を追加

--- MakeBat/MakeBat.py
+++ MakeBat/MakeBat.py
@@ -7,36 +7,39 @@
 ## コマンド
 - python _MakeBat.py [files] [options]
 - options:
-  - --pause : 完了時に待機する
-  - --cd : カレントディレクトリをファイルの親フォルダに移動してファイル名でコマンドを起動
-  - --self : 自分自身のbatを作成する
+  - --pause : 
+    - 完了時に待機する
+  - --cd : 
+    - カレントディレクトリをファイルの親フォルダに移動してファイル名でコマンドを起動
+  - --self : 
+    - 自分自身のbatを作成する
+  - --cmd_full : 
+    - pythonの実行コマンドに現在のpythonのフルパスを指定
+    - 未指定時にはpython launcherを使う
 '''
 
 import os
 import sys
 
-python = sys.executable
-myFile = os.path.abspath(__file__)
-
-def contents(py: str, options: list[str]):
+def contents(py: str, py_cmd: str, options: list[str]):
     if '--cd' in options:
         cdPath = os.path.dirname(py)
         pyName = os.path.basename(py)
         yield f'cd "{cdPath}" %*'
-        yield f'call "{python}" "{pyName}" %*'
+        yield f'call "{py_cmd}" "{pyName}" %*'
     else:
-        yield f'call "{python}" "{py}" %*'
+        yield f'call "{py_cmd}" "{py}" %*'
     if '--pause' in options:
         yield 'pause'
 
-def makeBat(py: str, options: list[str]):
+def make_bat(py: str, py_cmd: str, options: list[str]):
     ext = os.path.splitext(py)[-1]
     bn = py[0:-len(ext)] + '.bat'
     with open(bn, mode='w') as f:
-        for s in contents(py, options):
+        for s in contents(py, py_cmd, options):
             f.write(s + '\n')
 
-def mainA():
+def main():
     options = []
     files = []
     for s in sys.argv[1:]:
@@ -45,11 +48,15 @@ def mainA():
         else:
             files.append(s)
 
+    py_cmd = 'py'
     if '--self' in options:
-        files = [myFile]
+        my_file = os.path.abspath(__file__)
+        files = [my_file]
+    elif '--cmd_full' in options:
+        py_cmd = sys.executable
 
     for a in files:
-        makeBat(a, options)
+        make_bat(a, py_cmd, options)
 
 if __name__ == "__main__":
-    mainA()
+    main()
