c11444f9505c3f143f688501661f6904ec840a53
fkhd <fkhideaki@gmail.com>
2026-02-08 17:02:35 +0900

    オプション処理修正

--- MakeBat/MakeBat.py
+++ MakeBat/MakeBat.py
@@ -8,7 +8,7 @@
 - python _MakeBat.py [files] [options]
 - options:
   - --pause : 
-    - 完了時に待機する
+    - 完了時に待機するバッチファイルを出力する
   - --cd : 
     - カレントディレクトリをファイルの親フォルダに移動してファイル名でコマンドを起動
     - 未指定時は対象ファイルをフルパスで指定する
@@ -19,21 +19,24 @@
     - 未指定時にはpython launcherを使う
 '''
 
+from dataclasses import dataclass
 from pathlib import Path
 import sys
 
-def contents(py: Path, py_cmd: str, options: list[str]):
-    if '--cd' in options:
-        yield f'cd "{py.parent}"'
-        yield f'call "{py_cmd}" "{py.name}" %*'
-    else:
-        yield f'call "{py_cmd}" "{py.resolve()}" %*'
+@dataclass
+class Cfg:
+    py_cmd: str = ''
+    cd: bool = False
+    pause: bool = False
 
-def make_bat(py: Path, py_cmd: str, options: list[str]):
-    bn = py.parent / (py.stem + '.bat')
-    with open(bn, mode='w') as f:
-        for s in contents(py, py_cmd, options):
-            f.write(s + '\n')
+def make_bat(py: Path, cfg: Cfg):
+    if cfg.cd:
+        yield f'cd "{py.parent}"'
+        yield f'call "{cfg.py_cmd}" "{py.name}" %*'
+    else:
+        yield f'call "{cfg.py_cmd}" "{py.resolve()}" %*'
+    if cfg.pause:
+        yield 'pause'
 
 def main():
     options = []
@@ -44,17 +47,24 @@ def main():
         else:
             files.append(s)
 
-    py_cmd = 'py'
-    if '--self' in options:
-        files = [__file__]
-    elif '--cmd_full' in options:
-        py_cmd = sys.executable
+    cfg = Cfg()
+    cfg.py_cmd = 'py'
+    for s in options:
+        if s == '--cd':
+            cfg.cd = True
+        elif s == '--self':
+            files = [__file__]
+        elif s == '--cmd_full':
+            cfg.py_cmd = sys.executable
+        elif s == '--pause':
+            cfg.pause = True
 
     for a in files:
-        make_bat(Path(a), py_cmd, options)
-
-    if '--pause' in options:
-        yield 'pause'
+        py = Path(a)
+        bn = py.parent / (py.stem + '.bat')
+        with open(bn, mode='w') as f:
+            for s in make_bat(py, cfg):
+                f.write(s + '\n')
 
 if __name__ == "__main__":
     main()
