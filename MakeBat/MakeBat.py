a2bcd00544a7fae8265d098dff7f22c2933acbb4
fkhd <fkhideaki@gmail.com>
2026-05-09 14:11:21 +0900

    標準モードをカレントディレクトリ移動に変更

--- MakeBat/MakeBat.py
+++ MakeBat/MakeBat.py
@@ -9,9 +9,9 @@
 - options:
   - --pause
     - 完了時に待機するバッチファイルを出力する
-  - --cd
-    - カレントディレクトリをファイルの親フォルダに移動してファイル名でコマンドを起動
-    - 未指定時は対象ファイルをフルパスで指定する
+  - --abs
+    - 対象ファイルをフルパスで指定する
+    - 未指定時はカレントディレクトリをファイルの親フォルダに移動してファイル名でコマンドを起動
   - --self
     - 自分自身のbatを作成する
   - --cmd_full
@@ -28,16 +28,16 @@ import sys
 @dataclass
 class Cfg:
     py_cmd: str = ''
-    cd: bool = False
+    abs_path: bool = False
     pause: bool = False
 
 def contents(py: Path, cfg: Cfg, args: list[str]):
     arg = ' '.join([f'"{s}"' for s in args])
-    if cfg.cd:
+    if cfg.abs_path:
+        yield f'"{cfg.py_cmd}" "{py.resolve()}" {arg} %*'
+    else:
         yield f'cd /d "%~dp0"'
         yield f'"{cfg.py_cmd}" "{py.name}" {arg} %*'
-    else:
-        yield f'"{cfg.py_cmd}" "{py.resolve()}" {arg} %*'
     if cfg.pause:
         yield 'pause'
 
@@ -58,7 +58,7 @@ def main():
             files.append(s)
 
     cfg = Cfg()
-    cfg.cd = '--cd' in options
+    cfg.abs_path = '--abs' in options
     cfg.pause = '--pause' in options
 
     arg_files = '--arg_files' in options
