0c80d68d9c1587a3a8f34ad9aa4bf3054f0d9127
fkhd <fkhideaki@gmail.com>
2026-05-09 14:30:58 +0900

    クリップボードに直接ペーストするモード

--- RandomWord/RandomWord.py
+++ RandomWord/RandomWord.py
@@ -9,10 +9,22 @@
 - 各文字列は10文字または20文字の長さで生成される
 - 日付の接頭辞（YYYYMMDD形式）を付けた文字列も生成される
 - ユーザーはEnterキーを押すことで再度文字列を生成でき、'q'を入力すると終了する
+
+## コマンド
+- python RandomWord.py [options]
+- options:
+  - --clip
+    - リスト表示ではなくクリップボードに直接生成結果をセットする  
+      名称の入力を受けて  
+      日時_入力名_ランダム  
+      という形式で出力する
 '''
 
 import random, string
 import datetime
+import sys
+
+import pyperclip
 
 
 def rw(n):
@@ -37,7 +49,7 @@ def printRandom(count, length, prefix):
             print(r)
     print('--------------------')
 
-def main():
+def print_mode():
     todayYMD = getTodayYMD()
 
     while True:
@@ -49,6 +61,26 @@ def main():
 
         if input('Enter to retry (q to quit) >>') == 'q':
             break
+
+def clip_mode():
+    todayYMD = getTodayYMD()
+    name = input('name >> ')
+    r = rw(10)
+    if name:
+        s = f'{todayYMD}_{name}_{r}'
+    else:
+        s = f'{todayYMD}_{r}'
+    pyperclip.copy(s)
+    print(s)
+
+def main():
+    opt = sys.argv[1:]
+    clip = '--clip' in opt
+    if clip:
+        clip_mode()
+    else:
+        print_mode()
+
 
 if __name__ == "__main__":
     main()
